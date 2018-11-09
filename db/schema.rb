8a17ccd4fdf039bb12b294a72f09d9f432cb5a30
Bradley Froese <bfroese777@gmail.com>
2018-11-09 00:43:54 +0000

    Added category column to Products table to allow for product sorting and navigation by this column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_152853) do
+ActiveRecord::Schema.define(version: 2018_11_09_003937) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_152853) do
     t.string "shipping_info"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
 end
