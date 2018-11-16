fa63207fb77b0c236b8d5e80d0d2b9a45fcef8d4
Unknown <bfroese777@gmail.com>
2018-11-15 22:56:24 -0600

    Seed Script
    Got the seed script working with associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Purging Database..."
 Product.delete_all
 Category.delete_all
 AdminUser.delete_all
-Category.create(name: 'tea')
-Category.create(name: 'container')
+
+puts "Creating Admins..."
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-Category.where(name: 'tea').products.create(name: 'Lemon Ginger', description: 'A strongly flavoured herbal tea, with a hint of lemon', quantity: '25', price: '9.99', shipping_info: 'perishable')
-Category.where(name: 'tea').products.create(name: 'Vanilla Chai', description: 'A variant of chai tea flavoured with gentle notes of vanilla', quantity: '25', price: '9.99', shipping_info: 'perishable')
-Category.where(name: 'tea').products.create(name: 'Rooibos', description: 'A red tea made from the African Rooibos bush', quantity: '25', price: '9.99', shipping_info: 'perishable')
-Category.where(name: 'tea').products.create(name: 'Berry Blend', description: 'A mixture of raspberry, blackberry, and acai berry flavours', quantity: '25', price: '9.99', shipping_info: 'perishable')
-Category.where(name: 'container').products.create(name: 'Lobster Mug', description: 'A short mug with an image of a lobster on the side', quantity: '8', price: '4.99', shipping_info: 'fragile')
-Category.where(name: 'container').products.create(name: 'Flower Mug', description: 'A mediuim-sized mug with an image of a flower on the side', quantity: '5', price: '5.99', shipping_info: 'fragile')
-Category.where(name: 'container').products.create(name: 'Porcelain Cup', description: 'A petite and simplistic tea cup', quantity: '13', price: '4.99', shipping_info: 'fragile')
-Category.where(name: 'container').products.create(name: 'Porcelain Tea Pot', description: 'An elegant and minimalist tea pot', quantity: '5', price: '13.99', shipping_info: 'fragile')
-Category.where(name: 'container').products.create(name: 'Ceramic Cup', description: 'A hardy and reliable tea cup', quantity: '11', price: '4.99', shipping_info: 'fragile')
-Category.where(name: 'container').products.create(name: 'Ceramic Tea Pot', description: 'A strudy and tasteful tea pot', quantity: '6', price: '12.99', shipping_info: 'fragile')
+
+puts "Creating Categories..."
+tea = Category.create(name: 'tea')
+container = Category.create(name: 'container')
+
+puts "Creating Products..."
+tea.products.create(name: 'Lemon Ginger', description: 'A strongly flavoured herbal tea, with a hint of lemon', quantity: '25', price: '9.99', shipping_info: 'perishable')
+tea.products.create(name: 'Vanilla Chai', description: 'A variant of chai tea flavoured with gentle notes of vanilla', quantity: '25', price: '9.99', shipping_info: 'perishable')
+tea.products.create(name: 'Rooibos', description: 'A red tea made from the African Rooibos bush', quantity: '25', price: '9.99', shipping_info: 'perishable')
+tea.products.create(name: 'Berry Blend', description: 'A mixture of raspberry, blackberry, and acai berry flavours', quantity: '25', price: '9.99', shipping_info: 'perishable')
+
+
+container.products.create(name: 'Lobster Mug', description: 'A short mug with an image of a lobster on the side', quantity: '8', price: '4.99', shipping_info: 'fragile')
+container.products.create(name: 'Flower Mug', description: 'A mediuim-sized mug with an image of a flower on the side', quantity: '5', price: '5.99', shipping_info: 'fragile')
+container.products.create(name: 'Porcelain Cup', description: 'A petite and simplistic tea cup', quantity: '13', price: '4.99', shipping_info: 'fragile')
+container.products.create(name: 'Porcelain Tea Pot', description: 'An elegant and minimalist tea pot', quantity: '5', price: '13.99', shipping_info: 'fragile')
+container.products.create(name: 'Ceramic Cup', description: 'A hardy and reliable tea cup', quantity: '11', price: '4.99', shipping_info: 'fragile')
+container.products.create(name: 'Ceramic Tea Pot', description: 'A strudy and tasteful tea pot', quantity: '6', price: '12.99', shipping_info: 'fragile')
