27d2f919d578e1b23af06d872f8605c4ecaecc06
Bradley Froese <bfroese777@gmail.com>
2018-11-16 11:22:10 -0600

    Search Bar Tweaks
    trying to get the search bar to submit data correctly, but not quite working yet

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,7 +2,12 @@ class ProductController < ApplicationController
 	def index
 		@product_collection = Product.order('category_id').page(params[:page]).per(5)
 	end
+
 	def show
 		@product = Product.find(params[:id])
+	end
+
+	def search
+		@product_collection = Product.where("category_id = ?", params[:category_id])
 	end
 end
