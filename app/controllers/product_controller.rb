708454a7d784ec61e25316bd9a8966460b16dc74
Bradley Froese <bfroese777@gmail.com>
2018-11-22 13:08:42 -0600

    Search by Category and Keyword

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,13 +1,15 @@
 class ProductController < ApplicationController
 	def index
 		@product_collection = Product.order('category_id').page(params[:page]).per(5)
+		if params[:category].present?
+			@product_collection = Product.where("category_id = :cat AND (description LIKE :text OR name LIKE :text)", 
+												cat: params[:category][:category_id], 
+												text: "%" << params[:text] << "%"
+												).page(params[:page]).per(5)
+		end
 	end
 
 	def show
 		@product = Product.find(params[:id])
-	end
-
-	def search
-		@product_collection = Product.where("category_id = ?", params[:category_id])
 	end
 end
