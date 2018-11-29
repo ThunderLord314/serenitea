1dcb6bda559e5845974b94a882954092eeee48a1
Bradley Froese <bfroese777@gmail.com>
2018-11-29 11:14:04 -0600

    Product Controller Cleanup
    cleaned up the code in the product controller, moving some of the logic to another statement

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,13 +2,14 @@ class ProductController < ApplicationController
 	require 'date'
 	# Shows a list of all of the products, or if a search is submitted, products that match the search
 	def index
-		@product_collection = Product.order('category_id').page(params[:page]).per(5)
 		if params[:category].present?
-			sql = filter_sql(:filter)
-			@product_collection = Product.where(sql, 
+			filter_sql = sql_by_id(params[:filter])
+			@product_collection = Product.where(filter_sql, 
 												cat: params[:category][:category_id], 
 												text: "%" << params[:text] << "%"
 												).page(params[:page]).per(5)
+		else 
+			@product_collection = Product.order('category_id').page(params[:page]).per(5)
 		end
 	end
 
@@ -18,14 +19,15 @@ class ProductController < ApplicationController
 	end
 
 	# returns the sql for the filter that is selected
-	def filter_sql(id)
-		current_date = Date.today().prev_day(14).strftime('%Y-%m-%d %H-%M-%S')
-		filter_sql = "category_id = :cat AND (description LIKE :text OR name LIKE :text)"
+	def sql_by_id(id)
+		sql = "category_id = :cat AND (description LIKE :text OR name LIKE :text)"
 		if id == "2"
-			filter_sql << " AND sale_price != 0 AND price - sale_price > 0"
+			sql << " AND sale_price != 0 AND price - sale_price > 0"
 		end
 		if id == "3"
-			filter_sql << " AND created_at >= \'" << current_date << "\'"
+			current_date = Date.today().prev_day(14).strftime('%Y-%m-%d %H-%M-%S')
+			sql << " AND created_at >= \'" << current_date << "\'"
 		end
+		return sql
 	end
 end
